e8cd278036d4471baa5e45ff0a6d0b92786bcda8
Rafi Ferdos <rafiferdos@gmail.com>
2024-03-08 00:13:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className='container mx-auto w-11/12 lg:w-10/12'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
