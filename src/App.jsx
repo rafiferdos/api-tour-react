fbbf774b3415e10f3ee8eb6925a4ef380bf3ae56
Rafi Ferdos <rafiferdos@gmail.com>
2024-03-08 11:37:20 +0600

    Fix HTML title and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import './App.css'
 import Navbar from './Navbar/Navbar'
+import ScrollToTop from 'react-scroll-to-top'
+import Users from './Users/Users'
 
 function App() {
+  const loading = window.onload = () => {
+    setTimeout(() => {
+      document.getElementById('webpage').classList.remove('hidden')
+      document.getElementById('loader-container').classList.add('hidden')
+    }, 1500)
+  }
+  if (loading) {
+    return (
+      <div className='container mx-auto w-11/12 lg:w-10/12'>
+        <ScrollToTop className='!rounded-full !flex !justify-center !items-center ' smooth color="#6f00ff" />
+        <div id="loader-container" className="flex items-center justify-center min-h-screen">
+          <span className="items-center justify-center loading loading-infinity text-primary lg:w-20 w-14"></span>
+        </div>
+        <div id='webpage' className="hidden">
+          <Navbar />
 
-  return (
-    <div className='container mx-auto w-11/12 lg:w-10/12'>
-      <Navbar />
-    </div>
-  )
+          <Users />
+
+        </div>
+      </div>
+    )
+  }
+
+
+
+  // return (
+  //   // show loader for 1.5s and then hide it then show the return statement
+  //   // <div className='container mx-auto w-11/12 lg:w-10/12'>
+  //   //   <span id="loader" className="loading loading-infinity loading-lg"></span>
+  //   //   <Navbar />
+  //   // </div>
+  //   <div className='container mx-auto w-11/12 lg:w-10/12'>
+  //     <span id="loader" className="loading loading-infinity loading-lg"></span>
+  //     <Navbar />
+  //   </div>
+  // )
+
 }
 
 export default App
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                     </div>
                 </div>
                 <div className="navbar-center">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">API Tour</a>
                 </div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
